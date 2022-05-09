beb935ebe89eb0fab733da9e26e3fb8375e4cc66
Andrey Zakharchenko <a_zakharchenko_a@mail.ru>
2022-05-09 12:46:29 +0300

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,30 @@
+public class Worker {
+
+    private final OnTaskDoneListener callback;
+    private final OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("ERROR in task " + i);
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+}
